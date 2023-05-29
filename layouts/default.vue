82ca2bfc03ba5e06014f4b72bbc93d1f30e74dbe
Kyle Smith <kylesmith090502@gmail.com>
2023-05-29 16:58:32 -0400

    Added semantic layout

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="">
+    <div>
+      <h1>YSK Kyle Blog</h1>
+    </div>
+    <div>
+      <button>Home</button>
+      <button>Blog</button>
+      <button>Contact Me</button>
+    </div>
+  </header>
+  <main>
+    <slot />
+  </main>
+  <footer />
+</template>
