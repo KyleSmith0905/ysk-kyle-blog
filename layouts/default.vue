932b7eb37b561741052be5957f4c75f0d209b94f
Kyle Smith <kylesmith090502@gmail.com>
2023-06-06 14:35:28 -0400

    fix: Fixed scrollbar from not appearing.

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,7 +13,8 @@ const definedNuxtLink = computed(() => {
 const themeOverrides: GlobalThemeOverrides = {
   common: {
     borderRadius: '0.75rem',
-    modalColor: 'rgba(0, 0, 0, 0.8)'
+    modalColor: 'rgba(0, 0, 0, 0.8)',
+    scrollbarHeight: '100vh'
   }
 }
 
@@ -27,57 +28,59 @@ const { width } = useWindowSize()
   <NConfigProvider :theme="darkTheme" :theme-overrides="themeOverrides" class="flex min-h-full flex-col">
     <NMessageProvider>
       <NScrollbar class="h-screen" trigger="none">
-        <NDrawer v-model:show="drawerActive" :width="width">
-          <NDrawerContent title="Navigation" class="backdrop-blur-md" :closable="true">
-            <div class="flex flex-col gap-1">
-              <NButton :text-color="colors.white" :secondary="true" :tag="definedNuxtLink" to="/" class="justify-start">
-                <span>Home</span>
-              </NButton>
-              <NButton :text-color="colors.white" :secondary="true" :tag="definedNuxtLink" to="/blog" class="justify-start">
-                <span>Blog</span>
-              </NButton>
-              <NButton :text-color="colors.white" :secondary="true" :tag="definedNuxtLink" to="/contact" class="justify-start">
-                <span>contact Me</span>
-              </NButton>
+        <div class="flex min-h-screen flex-col">
+          <NDrawer v-model:show="drawerActive" :width="width">
+            <NDrawerContent title="Navigation" class="backdrop-blur-md" :closable="true">
+              <div class="flex flex-col gap-1">
+                <NButton :text-color="colors.white" :tag="definedNuxtLink" to="/" class="justify-start">
+                  <span>Home</span>
+                </NButton>
+                <NButton :text-color="colors.white" :tag="definedNuxtLink" to="/blog" class="justify-start">
+                  <span>Blog</span>
+                </NButton>
+                <NButton :text-color="colors.white" :tag="definedNuxtLink" to="/contact" class="justify-start">
+                  <span>contact Me</span>
+                </NButton>
+              </div>
+            </NDrawerContent>
+          </NDrawer>
+          <header class="fixed z-10 h-12 w-full font-display">
+            <div class="absolute -z-10 h-20 w-full bg-gradient-to-b from-black via-black to-transparent" />
+            <div class="absolute -z-10 h-24 w-full bg-rainbow-linear-gradient-r opacity-80 gradient-mask-b-0" />
+            <div class="mx-auto flex h-full w-11/12 max-w-4xl items-center justify-between">
+              <NuxtLink to="/" class="flex items-center gap-2">
+                <SiteLogo class="!h-8 !w-8" />
+                <h1 class="text-2xl">
+                  YSK Kyle Blog
+                </h1>
+              </NuxtLink>
+              <NButtonGroup class="hidden sm:block">
+                <NButton :text-color="colors.white" :secondary="true" :tag="definedNuxtLink" to="/">
+                  <span>Home</span>
+                </NButton>
+                <NButton :text-color="colors.white" :secondary="true" :tag="definedNuxtLink" to="/blog">
+                  <span>Blog</span>
+                </NButton>
+                <NButton :text-color="colors.white" :secondary="true" :tag="definedNuxtLink" to="/contact">
+                  <span>contact Me</span>
+                </NButton>
+              </NButtonGroup>
+              <div class="flex sm:hidden">
+                <NButton :secondary="true" @click="menuToggle()">
+                  <Icon name="solar:hamburger-menu-broken" size="32" />
+                </NButton>
+              </div>
             </div>
-          </NDrawerContent>
-        </NDrawer>
-        <header class="fixed z-10 h-12 w-full font-display">
-          <div class="absolute -z-10 h-20 w-full bg-gradient-to-b from-black via-black to-transparent" />
-          <div class="absolute -z-10 h-24 w-full bg-rainbow-linear-gradient-r opacity-80 gradient-mask-b-0" />
-          <div class="mx-auto flex h-full w-11/12 max-w-4xl items-center justify-between">
-            <NuxtLink to="/" class="flex items-center gap-2">
-              <SiteLogo class="!h-8 !w-8" />
-              <h1 class="text-2xl">
-                YSK Kyle Blog
-              </h1>
+          </header>
+          <main class="grow pt-6">
+            <slot />
+          </main>
+          <footer class="relative mt-8 block h-12 w-full text-center">
+            <NuxtLink to="/contact" class="text-center font-display text-lg">
+              Contact Me
             </NuxtLink>
-            <NButtonGroup class="hidden sm:block">
-              <NButton :text-color="colors.white" :secondary="true" :tag="definedNuxtLink" to="/">
-                <span>Home</span>
-              </NButton>
-              <NButton :text-color="colors.white" :secondary="true" :tag="definedNuxtLink" to="/blog">
-                <span>Blog</span>
-              </NButton>
-              <NButton :text-color="colors.white" :secondary="true" :tag="definedNuxtLink" to="/contact">
-                <span>contact Me</span>
-              </NButton>
-            </NButtonGroup>
-            <div class="flex sm:hidden">
-              <NButton :secondary="true" @click="menuToggle()">
-                <Icon name="solar:hamburger-menu-broken" size="32" />
-              </NButton>
-            </div>
-          </div>
-        </header>
-        <main class="grow pt-6">
-          <slot />
-        </main>
-        <footer class="relative mt-8 block h-12 w-full text-center">
-          <NuxtLink to="/contact" class="text-center font-display text-lg">
-            Contact Me
-          </NuxtLink>
-        </footer>
+          </footer>
+        </div>
       </NScrollbar>
     </NMessageProvider>
   </NConfigProvider>
